bc9646876555ce2855da31846dced36276a5a4a0
Jimmy Huang <jianiau.huang@gmail.com>
2014-09-29 03:07:09 +0800

    create INDEX file to record path

--- tool/mkromfs.c
+++ tool/mkromfs.c
@@ -33,12 +33,15 @@ void processdir(DIR * dirp, const char * curpath, FILE * outfile, const char * p
     uint32_t size, w, hash;
     uint8_t b;
     FILE * infile;
-
+	
+	
     while ((ent = readdir(dirp))) {
         strcpy(fullpath, prefix);
         strcat(fullpath, "/");
         strcat(fullpath, curpath);
         strcat(fullpath, ent->d_name);
+        
+
     #ifdef _WIN32
         if (GetFileAttributes(fullpath) & FILE_ATTRIBUTE_DIRECTORY) {
     #else
@@ -81,13 +84,54 @@ void processdir(DIR * dirp, const char * curpath, FILE * outfile, const char * p
     }
 }
 
+void createind (DIR * dirp, const char * curpath,const char * prefix,FILE * indfile) {
+    char fullpath[1024];
+    char romfspath[1024];
+    char indpath[1024];
+    struct dirent * ent;
+    DIR * rec_dirp;
+	
+	
+    while ((ent = readdir(dirp))) {
+        strcpy(fullpath, prefix);
+        strcat(fullpath, "/");
+        strcat(fullpath, curpath);
+        strcat(fullpath, ent->d_name);
+        
+		strcpy(romfspath, curpath);
+        strcat(romfspath, ent->d_name);
+    #ifdef _WIN32
+        if (GetFileAttributes(fullpath) & FILE_ATTRIBUTE_DIRECTORY) {
+    #else
+        if (ent->d_type == DT_DIR) {
+    #endif
+            if (strcmp(ent->d_name, ".") == 0)
+                continue;
+            if (strcmp(ent->d_name, "..") == 0)
+                continue;
+            strcat(fullpath, "/");
+            rec_dirp = opendir(fullpath);
+            createind(rec_dirp, fullpath + strlen(prefix) + 1, prefix,indfile);
+            closedir(rec_dirp);
+        } else {
+			strcpy(indpath, prefix);
+			strcat(indpath, "/INDEX");
+			if( strcmp(fullpath,indpath) != 0 )
+				fprintf(indfile,"%s\n",romfspath);
+        }
+    }
+	
+}
+
 int main(int argc, char ** argv) {
     char * binname = *argv++;
     char * o;
     char * outname = NULL;
-    char * dirname = ".";
+    char  indpath[1024];
+    char * dirname = ".";    
     uint64_t z = 0;
     FILE * outfile;
+    FILE * indfile;
     DIR * dirp;
 
     while ((o = *argv++)) {
@@ -123,7 +167,20 @@ int main(int argc, char ** argv) {
         perror("opening directory");
         exit(-1);
     }
+	strcpy(indpath, dirname);
+	strcat(indpath, "/INDEX");
 
+	indfile=fopen(indpath, "w");
+
+	if (!indfile) {
+		perror("create INDEX fail");
+		exit(-1);
+	}
+	createind(dirp,"",dirname,indfile);
+	fclose(indfile);
+	closedir(dirp);
+
+	dirp = opendir(dirname);
     processdir(dirp, "", outfile, dirname);
     fwrite(&z, 1, 8, outfile);
     if (outname)
